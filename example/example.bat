c564bd0ea6dfa598ad75fb2d18ac6fc25f170a6d
tzn <137900114@qq.com>
2022-09-07 19:08:22 +0800

    refactor assimp

--- example/example.bat
+++ example/example.bat
@@ -1,13 +1,21 @@
 cd ..
+
 if not exist build (
 	mkdir build
 	echo "build directory has been created"
 )
+
+if not exist %cd%\build\Release\Alex.exe goto rebuild
+if "%0" == "rebuild" goto rebuild
+goto rbend
+
+:rebuild 
 cmake -B build .
-cmake --build build
+cmake --build build --config Release
+:rbend
 
 if not exist %cd%\example\config (
-	.\build\Debug\Alex --spp 64 -o %cd%\example\output.png -s %cd%\example\scene.xml -d %cd%\example\config -nt 16
+	%cd%\build\Release\Alex --spp 64 -o %cd%\example\output.png -s %cd%\example\scene.xml -d %cd%\example\config -nt 16
 ) else (
-	.\build\Debug\Alex -c %cd%\example\config
+	%cd%\build\Release\Alex -c %cd%\example\config
 )
